2392e1e61c76c43ac8d093265fc1c9293d2248fc
NightFoxx254 <153968273+NightFoxx254@users.noreply.github.com>
2024-08-03 18:32:06 -0700

    rewriting the player function inside Main.py

--- Main.py
+++ Main.py
@@ -16,6 +16,7 @@ def move(j,inp, playahHand):
     mid[j] = inp
     playahHand.remove(inp)
     playahHand.append(random.randint(1,13))
+    print[mid]
 
 
 
@@ -32,46 +33,35 @@ while(gameOn):
     if moveable(compHand,mid) == False and moveable(compHand,mid) == False :
         mid[0] = random.randint(1,13)
         mid[1] = random.randint(1,13)
+        print("yodelayeehu")
 
     #Computer Move
     if(moveable(compHand,mid)):
         for i in range(5):#runs through the hand
             for j in range(2):#runs through mid
                 if compHand[i] == mid[j]+1 or compHand[i] == mid[j]-1:#checks if the current card in the hand can be played in the current position of mid
-                    print(mid[j])
-                    mid[j] = compHand[i]#places the card down
-                    print(mid[j])
-                    compHand.pop(i)#removes the placed card from the hand
-                    compHand.append(random.randint(1,13))#dhraws a new card into the hand
-                    print("beep")
+                    move(j,compHand[i],compHand)
 
-    streak = True
-    while(streak):
-        if moveable(playahHand,mid) == False:
-            streak = False
-            break
+    #Player Move
+    while(moveable(playahHand,mid)):
+        print("Your Hand:")
+        print(playahHand)
+        print("The Middle Section:")
+        print(mid)
+
+        print("What card do you want to play?")
+        ining = int(input(">"))
+        print("Left pile(input 1) or right pile(input 2)")
+        to = int(input(">"))
+        to-=1
+
+        if mid[to] == 13:
+            if ining == 12 or ining == 1:
+                move(to,ining,playahHand)
+        elif mid[to] == 1:
+            if ining == 2 or ining == 13:
+                move(to,ining,playahHand)
+        elif mid[to]+1 == ining or mid[to]-1 == ining:
+            move(to,ining,playahHand)
         else:
-            print(mid[0],mid[1])
-            print(playahHand[0],playahHand[1],playahHand[2],playahHand[3],playahHand[4])
-            print("What card do you want to play?")
-            inp = int(input(">"))
-            if inp in playahHand:
-                if mid[0] == 13:
-                    if inp == 1 or inp == 12:
-                        move(0,inp,playahHand)
-                elif mid[1] == 13:
-                    if inp == 1 or inp == 12:
-                        move(1,inp,playahHand)
-                if mid[0] == 1:
-                    if inp == 2 or inp == 13:
-                        move(0,inp,playahHand)
-                elif mid[1] == 1:
-                    if inp == 2 or inp == 13:
-                        move(1,inp,playahHand)
-                elif mid[0]+1 == inp or mid[0]-1 == inp:
-                    move(0,inp,playahHand)
-                elif mid[1]+1 == inp or mid[1]-1 == inp:
-                    move(1,inp,playahHand)
-                else:
-                    print("sorry that is an invalid move")
-
+            print("invalid move :(")
